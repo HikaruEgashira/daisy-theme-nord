d127de0f54634687a185c89ddc7f52a768f2235c
HikaruEgashira <s1811320@gmail.com>
2021-08-09 12:41:44 +0900

    add example

---
+++ example/tailwind.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, transform } from "windicss/helpers";
+import nord from "daisy-theme-nord";
+//@ts-ignore
+import colors from "daisyui/colors";
+
+export default defineConfig({
+  plugins: [transform("daisyui")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  daisyui: {
+    themes: [nord],
+  },
+});
